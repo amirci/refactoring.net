6e8667fe8042c9b26428a157270530277874c31a
Amir Barylko <amir@barylko.com>
2014-04-06 12:06:23 -0600

    Added example 1

---
+++ src/Refactoring/Example1/MovieCriticScore.cs
@@ -0,0 +1,39 @@
+﻿namespace Refactoring.Example1
+{
+    public class MovieCriticScore
+    {
+        private readonly bool _isWellKnown;
+        private readonly bool _isKnownAbroad;
+
+        public MovieCriticScore(bool isWellKnown=false, bool isKnownAbroad=false)
+        {
+            _isWellKnown = isWellKnown;
+            _isKnownAbroad = isKnownAbroad;
+        }
+
+        public int FinalScore()
+        {
+            var result = 1;
+
+            if (_isWellKnown)
+            {
+                result = LocalScore();
+                if (_isKnownAbroad)
+                {
+                    result += InternationalScore();
+                }
+            }
+            return result;
+        }
+
+        private static int InternationalScore()
+        {
+            return 10;
+        }
+
+        private static int LocalScore()
+        {
+            return 10;
+        }
+    }
+}
---
+++ test/Refactoring.Tests/Example1/MovieCriticScoreTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Refactoring.Example1;
+
+namespace Refactoring.Tests.Example1
+{
+    public class MovieCriticScoreTest
+    {
+        public class CalculateFinalScoreMethod
+        {
+            protected int _score;
+
+            public class When_the_movie_is_known_locally : CalculateFinalScoreMethod
+            {
+                [Test]
+                public void Then_the_score_is_medium()
+                {
+                    var calculator = new MovieCriticScore(true);
+
+                    this._score = calculator.FinalScore();
+
+                    Assert.That(_score, Is.EqualTo(10));
+                }
+            }
+
+            public class When_the_movie_is_known_abroad : CalculateFinalScoreMethod
+            {
+                [Test]
+                public void Then_the_score_is_high()
+                {
+                    var calculator = new MovieCriticScore(true, true);
+
+                    this._score = calculator.FinalScore();
+
+                    Assert.That(_score, Is.EqualTo(20));
+                }
+            }
+
+            public class When_the_movie_is_not_known_at_all : CalculateFinalScoreMethod
+            {
+                [Test]
+                public void Then_the_score_is_low()
+                {
+                    var calculator = new MovieCriticScore();
+
+                    this._score = calculator.FinalScore();
+
+                    Assert.That(_score, Is.EqualTo(1));
+                }
+            }
+        }
+    }
+}
