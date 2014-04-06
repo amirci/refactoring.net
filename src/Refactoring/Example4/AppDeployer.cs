f4ca37e86e7918cdad78b4b5f1d98d016f129a4a
Amir Barylko <amir@barylko.com>
2014-04-06 13:47:55 -0600

    Added AppDeployer and a deploy method

---
+++ src/Refactoring/Example4/AppDeployer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Refactoring.Example4
+{
+    public class AppDeployer
+    {
+        public IEnumerable<string> Deploy(IEnumerable<string> environments)
+        {
+            var result = new List<string>();
+
+            foreach (var target in environments)
+            {
+                result.Add("Deployed to " + target);   
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ test/Refactoring.Tests/Example4/AppDeployerTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Refactoring.Example4;
+
+namespace Refactoring.Tests.Example4
+{
+    class AppDeployerTest
+    {
+        class DeployMethod
+        {
+            [Test]
+            public void When_calling_with_multiple_environments()
+            {
+                var deployer = new AppDeployer();
+
+                var environments = new [] { "Production", "Staging", "UAT" };
+
+                var actual = deployer.Deploy(environments);
+
+                var expected = new[] {"Deployed to Production", "Deployed to Staging", "Deployed to UAT"};
+
+                Assert.That(actual, Is.EquivalentTo(expected));
+            }
+        }
+    }
+}
