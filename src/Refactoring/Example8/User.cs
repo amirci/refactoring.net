b2471f693f99a4d4b1014c3b6135ac99c03d122c
Amir Barylko <amir@barylko.com>
2014-04-08 06:16:31 -0600

    Added NullUser to handle not available responses

--- src/Refactoring/Example8/User.cs
+++ src/Refactoring/Example8/User.cs
@@ -2,7 +2,20 @@
 {
     public class User
     {
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string PhoneNumber { get; set; }
+    }
+
+    public class NullUser : User
+    {
+        public override string Name
+        {
+            get { return "n/a"; }
+        }
+
+        public override string PhoneNumber
+        {
+            get { return "n/a";  }
+        }
     }
 }
