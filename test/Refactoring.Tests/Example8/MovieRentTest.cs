f35f1346ba7732d22934849156e81a9e7d32fd58
Amir Barylko <amir@barylko.com>
2014-04-08 06:12:49 -0600

    Added MovieRent to model renting a movie to a user

---
+++ test/Refactoring.Tests/Example8/MovieRentTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Refactoring.Example5;
+using Refactoring.Example8;
+
+namespace Refactoring.Tests.Example8
+{
+    class MovieRentTest
+    {
+        protected DateTime _start;
+        protected Movie _movie;
+
+        protected MovieRent Subject { get; set; }
+
+        public class UserNameMethod : MovieRentTest
+        {
+            [Test]
+            public void When_the_user_is_unknown()
+            {
+                this.Subject = new MovieRent(_movie, _start, null);
+
+                this.Subject.UserName().Should().Be("n/a");
+            }
+
+            [Test]
+            public void When_the_user_is_known()
+            {
+                this.Subject = new MovieRent(_movie, _start, new User {Name = "Sean Morter"});
+
+                this.Subject.UserName().Should().Be("Sean Morter");
+            }
+        }
+
+        public class UserPhoneNumberMethod : MovieRentTest
+        {
+            [Test]
+            public void When_xxxxx()
+            {
+                // pending test please complete
+            }
+        }
+    }
+}
